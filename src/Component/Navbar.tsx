f1fe749bc694b63397016674ca152c5fe34eeac2
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-02-22 09:42:43 +0700

    SideNavbar

--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -1,4 +1,9 @@
-import { AiFillLinkedin, AiFillGithub, AiOutlineSearch } from "react-icons/ai";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiOutlineSearch,
+  AiOutlineClose,
+} from "react-icons/ai";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 type Props = {};
@@ -9,20 +14,29 @@ const Navbar = (props: Props) => {
       <div className="flex">
         <RxHamburgerMenu
           size={30}
-          className="text-primary-400 w-fit h-fit pt-9 mr-10"
+          className="text-primary-400 w-fit h-fit pt-9 md:mr-10"
         />
-        <h1 className="text-xl lg:text-3xl align-middle my-auto w-fit h-fit py-4 text-primary-400 font-bold">
+        {/* Side Nav */}
+        <div className="bg-primary-100/80 fixed w-full h-screen z-20 inset-0"></div>
+        <div className="bg-primary-200 fixed inset-0 z-50 h-screen w-[350px] duration-300">
+          <AiOutlineClose
+            size={30}
+            className="text-primary-400 absolute top-4 font-bold right-4"
+          />
+        </div>
+        {/* Side Nav */}
+        <h1 className="text-xl hidden md:flex lg:text-3xl align-middle my-auto w-fit h-fit py-4 text-primary-400 font-bold">
           A K A R A S H I
         </h1>
       </div>
-      <div className="mx-auto my-auto">
+      <div className="my-auto ml-[-55px] md:ml-0">
         <div className="relative ml-5">
           <div className="absolute top-3 left-4 ">
             <AiOutlineSearch size={18} className="text-primary-400" />
           </div>
           <input
             type="text"
-            className="w-[50vw] max-w-[600px] h-[40px] bg-primary-200 text-primary-400 rounded-xl pl-10 justify-center"
+            className="w-[230px] md:w-[350px] lg:w-[400px] xl:w-[500px] 2xl:w-[700px] h-[40px] bg-primary-200 text-primary-400 rounded-xl pl-10 justify-center"
             placeholder="Search A Game"
           />
         </div>
