315866b02826958c015e433c674a0bac2ad158d3
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-05-18 14:46:59 +0700

    feat: Adding More Dummy Data For Card

--- src/Dummy/Genres.ts
+++ src/Dummy/Genres.ts
@@ -1,4 +1,4 @@
-interface Platform {
+interface Genres {
   platform: string;
   image: string;
   popular_items: string;
@@ -9,23 +9,23 @@ interface Platform {
   game5: string;
 }
 
-const Platform: Platform[] = [
+const Genres: Genres[] = [
   {
-    platform: "PC",
+    platform: "Action",
     image:
       "https://images.hdqwalls.com/download/battlefield-4-soldier-to-1280x720.jpg",
-    popular_items: "120,456",
-    game1: "Counter Strike: Global Offensive",
-    game2: "Portal 2",
-    game3: "VALORANT",
+    popular_items: "172,840",
+    game1: "Red Dead Redemption 2",
+    game2: "Cyberpunk 2077",
+    game3: "Uncharted 3",
     game4: "NieR: Automata",
-    game5: "DrakenGard 3",
+    game5: "Batlefield 4",
   },
 
   {
     platform: "Playstation",
     image: "https://wallpaperaccess.com/full/3690445.jpg",
-    popular_items: "10,567",
+    popular_items: "105,357",
     game1: "God Of War: Ragnarok",
     game2: "Uncharted 4: The Thieves End",
     game3: "Spiderman",
@@ -36,7 +36,7 @@ const Platform: Platform[] = [
     platform: "XBOX 1 X/S",
     image:
       "https://wallpapersmug.com/download/1024x768/cc9656/halo-infinite-2020-soldier.jpg",
-    popular_items: "2,209",
+    popular_items: "13,765",
     game1: "God Of War: Ragnarok",
     game2: "Uncharted 4: The Thieves End",
     game3: "Spiderman",
@@ -47,7 +47,7 @@ const Platform: Platform[] = [
     platform: "SWITCH",
     image:
       "https://1.bp.blogspot.com/-LSmZDfZrN6M/V9Md5aQ1fiI/AAAAAAAAAcM/uRT2AIayhSUEP48GeHVebuEfxJ8aa_tVgCLcB/s1600/the_legend_of_Zelda-horse-sunlight-link-nintendo-wii-u-game-wallpapers.jpg",
-    popular_items: "879",
+    popular_items: "9.120",
     game1: "The Legend Of Zelda: Tears Of The Kingdom",
     game2: "The Legend Of Zelda: Breath Of The Wild",
     game3: "Animal Crossing",
