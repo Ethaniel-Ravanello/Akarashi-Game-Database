09392f2a2bd9d59256c2b96b4efebb5ad6cd1e2f
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-05-24 09:39:14 +0700

    fix: Revamp Dummy Data adding ID to the game

--- src/Dummy/Tags.ts
+++ src/Dummy/Tags.ts
@@ -2,9 +2,21 @@ interface Tags {
   platform: string;
   image: string;
   popular_items: string;
-  game1: string;
-  game2: string;
-  game3: string;
+  game1: {
+    name: string;
+    player: string;
+    id: number;
+  };
+  game2: {
+    name: string;
+    player: string;
+    id: number;
+  };
+  game3: {
+    name: string;
+    player: string;
+    id: number;
+  };
 }
 
 const Tags: Tags[] = [
@@ -12,34 +24,82 @@ const Tags: Tags[] = [
     platform: "Singleplayer",
     image: "https://wallpapercave.com/wp/wp11318812.jpg",
     popular_items: "120,456",
-    game1: "Elden Ring",
-    game2: "Bloodborne",
-    game3: "NieR: Automata",
+    game1: {
+      name: "Elden Ring",
+      player: "5,833",
+      id: 326243,
+    },
+    game2: {
+      name: "Bloodborne",
+      player: "8,098",
+      id: 3387,
+    },
+    game3: {
+      name: "NieR:Automata",
+      player: "7,772",
+      id: 10141,
+    },
   },
 
   {
     platform: "Mulitplayer",
     image: "https://wallpapercave.com/wp/wp9041296.jpg",
     popular_items: "10,567",
-    game1: "VALORANT",
-    game2: "Counter Strike: Global Offensive",
-    game3: "Splatoon 3",
+    game1: {
+      name: "Valorant",
+      player: "763",
+      id: 415171,
+    },
+    game2: {
+      name: "Counter-Strike: Global Offensive",
+      player: "15,182",
+      id: 4291,
+    },
+    game3: {
+      name: "Splatoon 3",
+      player: "132",
+      id: 558975,
+    },
   },
   {
     platform: "RPG",
     image:
       "https://wallpapersmug.com/download/1024x768/cc9656/halo-infinite-2020-soldier.jpg",
     popular_items: "2,209",
-    game1: "The Elder Scrolls 5: Skyrim",
-    game2: "Disco Elysium",
-    game3: "Tale Of Arises",
+    game1: {
+      name: "The Elder Scroll V: Skyrim",
+      player: "14,658",
+      id: 5679,
+    },
+    game2: {
+      name: "Disco Elysium",
+      player: "6,389",
+      id: 262382,
+    },
+    game3: {
+      name: "Tale Of Arises",
+      player: "1,321",
+      id: 326244,
+    },
   },
   {
     platform: "Exploration",
     image: "https://wallpapercave.com/wp/wp11520704.jpg",
     popular_items: "879",
-    game1: "Star Wars Jedi: Survivor",
-    game2: "Sons Of The Forest",
-    game3: "Hogwarts Legacy",
+    game1: {
+      name: "Star Wars Jedi: Survivor",
+      player: "483",
+      id: 793647,
+    },
+    game2: {
+      name: "Sons of the forest",
+      player: "214",
+      id: 398405,
+    },
+    game3: {
+      name: "Hogwarts Legacy",
+      player: "1,690",
+      id: 398405,
+    },
   },
 ];
